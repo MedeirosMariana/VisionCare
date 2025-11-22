9ea59405213899aa93bc282ca933e66f25e82299
Mariana Medeiros <marianacostamedeiros1@gmail.com>
2025-11-22 01:19:44 -0300

    - Criado classes de exceções para padronização das mesmas.

---
+++ src/main/java/com/project/visioncare/exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.project.visioncare.exceptions;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
